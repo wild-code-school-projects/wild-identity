faceb0ce63b66f20450b26489b498e64a892eb31
SuperCoolNinja <sbpro570@gmail.com>
2024-05-23 20:54:04 +0200

    init

---
+++ Controllers/PrivateZoneController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAuth.Controllers
+{
+    [Authorize]
+    public class PrivateZoneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Bienvenue dans la zone priver !");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
